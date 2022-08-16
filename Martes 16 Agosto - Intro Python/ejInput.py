df9768527e15886e7e53d31170aba892896fd6d7
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2022-08-16 13:58:19 -0500

    Ejercicios basicos Python

---
+++ Martes 16 Agosto - Intro Python/ejInput.py
@@ -0,0 +1,3 @@
+edad = int(input("Dime tu edad: "))
+print(edad)
+print(type(edad))
---
+++ Martes 16 Agosto - Intro Python/ejLib.py
@@ -0,0 +1,9 @@
+import math
+print(math.pi)
+print(math.e)
+
+import random
+a = random.randint(10,90)
+b = random.randint(50,87)
+suma = a + b
+print(f"{a} + {b} = {suma}")
